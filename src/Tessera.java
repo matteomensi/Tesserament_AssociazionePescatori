f248cc207c94b78f2161333a6202916a43681878
matteo <mat.mensi@gmail.com>
2018-05-04 10:31:21 +0200

    Aggiuntadiagrammi

--- src/Tessera.java
+++ src/Tessera.java
@@ -1,4 +1,85 @@
 
-public class Tessera {
+public class Tessera 
+{
+	//Attributi
+	private int codiceIdentificativo;
+	private String nome;
+	private String cognome;
+	private String codiceFiscale;
+	private String dataNascita;
+	private String info;
+	
+	public Tessera (int codiceIdentificativo,String nome, String cognome,
+			String codiceFiscale, String dataNascita, String info)
+	{
+		this.codiceIdentificativo=codiceIdentificativo;
+		this.nome=nome;
+		this.cognome=cognome;
+		this.codiceFiscale=codiceFiscale;
+		this.dataNascita=dataNascita;
+		this.info=info;
+	}
+	
+	public int getCodiceIdentificativo() 
+	{
+		return codiceIdentificativo;
+	}
+	
+	public void setCodiceIdentificativo(int codiceIdentificativo) 
+	{
+		this.codiceIdentificativo = codiceIdentificativo;
+	}
+
+	public String getNome() 
+	{
+		return nome;
+	}
+
+	public void setNome(String nome) 
+	{
+		this.nome = nome;
+	}
+
+	public String getCognome() 
+	{
+		return cognome;
+	}
+
+	public void setCognome(String cognome) 
+	{
+		this.cognome = cognome;
+	}
+
+	public String getCodiceFiscale() 
+	{
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) 
+	{
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public String getDataNascita() 
+	{
+		return dataNascita;
+	}
+
+	public void setDataNascita(String dataNascita) 
+	{
+		this.dataNascita = dataNascita;
+	}
+
+	public String getInfo() 
+	{
+		return info;
+	}
+
+	public void setInfo(String info) 
+	{
+		this.info = info;
+	}
+	
+	
 
 }
